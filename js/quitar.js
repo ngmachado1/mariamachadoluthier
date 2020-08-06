5ed9c39f4cfe3f7a5cb39ef11e81932ec84a3452
ngmachado1 <54647104+ngmachado1@users.noreply.github.com>
2020-08-06 10:41:05 -0300

    cambio2

--- js/quitar.js
+++ js/quitar.js
@@ -2,11 +2,18 @@ window.onscroll = function (){
     const nav = document.querySelector ('.fix');
     const imagen = document.querySelector ('.header-logo')
 
-    if(window.pageYOffset > 400){
-        nav.classList.add('dark')
-        imagen.classList.add('pequena')
-    }else{
-        nav.classList.remove('dark')
-        imagen.classList.remove('pequena')
+    if(screen.width<762){
+        imagen.classList.add('pequena');
+    } else {
+        if(window.pageYOffset > 400){
+            nav.classList.add('dark')
+            imagen.classList.add('pequena')
+        }else{
+            nav.classList.remove('dark')
+            imagen.classList.remove('pequena')
         }
+    }
+
+
+
 }
